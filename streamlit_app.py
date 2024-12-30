3a44e2a255599ae1f8b5228c5ebbe04e998d3989
Mahfudin777 <mahfudin@students.esqbs.ac.id>
2024-12-31 00:54:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,8 +36,9 @@ def main():
 
     with st.form(key='my_form'):
         raw_text = st.text_area("Type Here")
-        submit_text = st.form_submit_button(label='Submit')
         target_language = st.text_area("Type Here")
+        submit_text = st.form_submit_button(label='Submit')
+        
 
        
     translated_text = translate_text(raw_text, target_language)
